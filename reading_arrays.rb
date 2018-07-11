98ed3a5b92ee768a663ac2e2e561d1c83561a432
shakishaki <shaki.canchola@yahoo.com>
2018-07-11 16:37:30 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -19,7 +19,7 @@ end
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
-  puts student_names.first_student_by_index
+  puts student_names.last_student_by_index
 end
 
 def first_student_by_method
